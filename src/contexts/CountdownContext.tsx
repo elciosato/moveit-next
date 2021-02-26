2c3a59e768ad1993a2b57167382d009db85f671f
Elcio Yoshiaki Sato <elciosato@gmail.com>
2021-02-26 18:13:47 +0100

    Adjust Countdown Timer to 25 min

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge } = useContext(ChallengeContext);
 
     // 25min in seconds
-    const [time, setTime] = useState(0.1 * 60); 
+    const [time, setTime] = useState(25 * 60); 
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
